178a5dcdef0e8a8b1b19bf6a29beb808e56c28d6
Heavenstone <malolegendrelemaire@gmail.com>
2020-05-05 17:08:15 +0200

    test

--- src/app.tsx
+++ src/app.tsx
@@ -4,15 +4,17 @@ import { hot } from "react-hot-loader/root";
 
 const Index = lazy(() => import("~/pages/index"));
 
-export const App = () => <div>
-  <Suspense fallback={<div>Loading...</div>}>
-    <Switch>
-      <Route
-      exact
-      path="/"
-      component={Index}
-      />
-    </Switch>
-  </Suspense>
-</div>
+export const App = () => (
+  <div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route
+        exact
+        path="/"
+        component={Index}
+        />
+      </Switch>
+    </Suspense>
+  </div>
+)
 export default hot(App);
