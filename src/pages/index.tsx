7875ed215c076529f606b62a06fe5e602b7e46a3
Heavenstone <malolegendrelemaire@gmail.com>
2020-05-05 22:08:04 +0200

    added a print line

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,11 +12,11 @@ const Index: FunctionComponent = () => {
           <h1
           className="
           font-bold text-2xl bg-gray-700
-          inline-block px-4 py-1
+          inline-block px-4 py-1 align-middle leading-10
           -ml-10 mt-5 rounded
           "
           >
-            Heav Faxer <Icon path={mdiFax} />
+            Heav Faxer <Icon className="inline-block h-10" path={mdiFax} />
           </h1>
         </section>
         <section className={`${elsClasses} h-64 w-64`}>
@@ -25,8 +25,17 @@ const Index: FunctionComponent = () => {
         </section>
       </div>
       <div className="flex">
-        <section className={`${elsClasses} flex-grow h-64`}>
-
+        <section className={`${elsClasses}
+        flex-grow h-64 p-10
+        flex justify-center items-center
+        `}>
+          <div className="bg-gray-800 h-5 rounded-full w-full relative">
+            <div className="absolute inset-0 overflow-hidden rounded-full">
+              <div className="absolute top-0 right-0 left-0 h-2 bg-gray-900" />
+            </div>
+          </div>
+        </section>
+        <section className={`${elsClasses} w-64`}>
         </section>
       </div>
     </article>
