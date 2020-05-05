9cc17292c7ea6e8f49f1fd4fd86c7f9b03ff11ef
Heavenstone <malolegendrelemaire@gmail.com>
2020-05-05 19:48:34 +0200

    added react-dropzone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import { useState, useCallback } from "react";
+import { useDropzone } from 'react-dropzone';
 
 const Index = () => {
   const [stateImg, setStateImg] = useState("mailbox-empty");
-
+  
   return <div className="bg-black p-10 text-xl sm:min-w-96">
     <div className="flex -mx-6 -mt-8 mb-5">
       <h1 className="text-2xl text-white">Heav Faxer</h1>
@@ -10,16 +11,6 @@ const Index = () => {
       <span>
         <img src={`/emojies/${stateImg}.png`} className="ml-2 w-6 inline-block" />
       </span>
-    </div>
-    <div className="flex flex-row justify-center flex-wrap -mr-10 -mb-10">
-      <div className="mr-10 mb-10 flex flex-col justify-center items-center">
-        <div>Envoyer un fichier</div>
-        <span><img src="/emojies/file_folders.png" alt="file folder" /></span>
-      </div>
-      <div className="mr-10 mb-10 flex flex-col justify-center items-center">
-        <div>Racourcir un lien</div>
-        <span><img src="/emojies/ticket.png" alt="ticket" /></span>
-      </div>
     </div>
   </div>
 };
