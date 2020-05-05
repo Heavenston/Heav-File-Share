7fb0b08c58c4a939310f479d0b5309fd92d02915
Heavenstone <malolegendrelemaire@gmail.com>
2020-05-05 19:12:19 +0200

    renamed things

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
+import { useState, useCallback } from "react";
 
-const Index = () => <div className="flex items-center justify-center">
-  
-</div>;
+const Index = () => {
+  const [stateImg, setStateImg] = useState("mailbox-empty");
+
+  return <div className="bg-black p-10 text-xl min-w-96">
+    <div className="flex -mx-6 -mt-8 mb-5">
+      <h1 className="text-2xl text-white">Heav Faxer</h1>
+      <span className="flex-grow" />
+      <span>
+        <img src={`/emojies/${stateImg}.png`} className="ml-2 w-6 inline-block" />
+      </span>
+    </div>
+    <div className="flex flex-col items-center">
+      
+    </div>
+  </div>
+};
 export default Index;
