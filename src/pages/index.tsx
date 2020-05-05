a60548cb7b197c1a66f4ae18672d20b37d6a9a0a
Heavenstone <malolegendrelemaire@gmail.com>
2020-05-05 19:26:10 +0200

    change images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { useState, useCallback } from "react";
 const Index = () => {
   const [stateImg, setStateImg] = useState("mailbox-empty");
 
-  return <div className="bg-black p-10 text-xl min-w-96">
+  return <div className="bg-black p-10 text-xl sm:min-w-96">
     <div className="flex -mx-6 -mt-8 mb-5">
       <h1 className="text-2xl text-white">Heav Faxer</h1>
       <span className="flex-grow" />
@@ -11,12 +11,14 @@ const Index = () => {
         <img src={`/emojies/${stateImg}.png`} className="ml-2 w-6 inline-block" />
       </span>
     </div>
-    <div className="flex flex-row justify-center">
-      <div className="mr-10">
-        <img src="/emojies/file_folders.png" alt="file folder" />
+    <div className="flex flex-row justify-center flex-wrap -mr-10 -mb-10">
+      <div className="mr-10 mb-10 flex flex-col justify-center items-center">
+        <div>Envoyer un fichier</div>
+        <span><img src="/emojies/file_folders.png" alt="file folder" /></span>
       </div>
-      <div>
-        <img src="/emojies/ticket.png" alt="ticket" />
+      <div className="mr-10 mb-10 flex flex-col justify-center items-center">
+        <div>Racourcir un lien</div>
+        <span><img src="/emojies/ticket.png" alt="ticket" /></span>
       </div>
     </div>
   </div>
